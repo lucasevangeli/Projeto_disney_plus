dc10dc4c828d7bf6244262c62c82154d259bb430
Lucas Evan <lucasevan478@gmail.com>
2024-01-12 16:03:01 -0300

    finalizada duas section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     
 
@@ -16,7 +17,18 @@ document.addEventListener('DOMContentLoaded', function(){
             
         })
     }
+
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -26,6 +38,8 @@ function removeBotaoAtivo(){
         
 
     }
+
+    
 }
 
 
